e615e29c7325091a23045bff41e461ef433ac46d
Abel <ab.van.der.steen@sintlucasedu.nl>
2022-11-03 14:28:05 +0100

    responsiveness and language switch

--- js/myscript.js
+++ js/myscript.js
@@ -1,9 +1,11 @@
 var txtTitle = '  <Abel_van_der_Steen>';
-var txtDescription = "Hello and welcome to my Portfolio Website! I am a 17 year-old Software Development student and am very passionate about what I do. I hope you'll be able to find what you're looking for here as I delve deeper into all things me and my skills!";
+var txtDescriptionEN = "Hello and welcome to my Portfolio Website! I am a 17 year-old Software Development student and am very passionate about what I do. I hope you'll be able to find what you're looking for here as I delve deeper into all things me and my skills!";
+var txtDescriptionNL = "Hallo en welkom bij mijn Portfolio Website! Ik ben een 17 jarige Sofware Development student en ben vol passie voor mijn vak. Ik hoop dat u wat u zoekt hier zal kunnen vinden terwijl we dieper in alles over mij en mijn skills duiken!";
 var speed1 = 90;
 var speed2 = 10;
 i = 0; 
 o = 0;
+u = 0;
 
 function typeWriter() {
     if (i < txtTitle.length) {
@@ -13,18 +15,32 @@ function typeWriter() {
     }
 }
 
-function typeWriter2() {
-    if (o < txtDescription.length) {
-        document.getElementById("description").innerHTML += txtDescription.charAt(o);
-        setTimeout(typeWriter2, speed2);
+function typeWriter2_en() {
+    if (o < txtDescriptionEN.length) {
+        document.getElementById("description").innerHTML += txtDescriptionEN.charAt(o);
+        setTimeout(typeWriter2_en, speed2);
+        o++;
+    }
+}
+function typeWriter2_nl() {
+    if (o < txtDescriptionNL.length) {
+        document.getElementById("description").innerHTML += txtDescriptionNL.charAt(o);
+        setTimeout(typeWriter2_nl, speed2);
         o++;
     }
 }
 
 window.onload = function() {
     ChangeText();
-    typeWriter()
-    typeWriter2();
+    typeWriter();
+
+    var path = window.location.pathname;
+    var page = path.split("/").pop();
+    if (page == "index_nl.html") {
+        typeWriter2_nl();
+    } else {
+        typeWriter2_en();
+    }
 }
 
 function delay(time) {
